40fd63f55b662bdda33eac1cea716bec96d7b4f0
Shota Imaki <shota.imaki.0801@gmail.com>
2021-02-16 13:08:14 +0900

    Create minimal implementation (close #1)

---
+++ main.py
@@ -0,0 +1,419 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn
+import torch
+import torch.nn.functional as fn
+from torch.optim import Adam
+from tqdm import tqdm
+
+from utils import Net
+from utils import bs_delta
+from utils import cash_equivalent
+from utils import clamp
+from utils import entropic_loss
+from utils import generate_gbm
+
+seaborn.set_style("whitegrid")
+
+FONTSIZE = 18
+matplotlib.rcParams["figure.figsize"] = (10, 5)
+matplotlib.rcParams["figure.dpi"] = 300
+matplotlib.rcParams["figure.titlesize"] = FONTSIZE
+matplotlib.rcParams["font.family"] = "sans-serif"
+matplotlib.rcParams["legend.fontsize"] = FONTSIZE
+matplotlib.rcParams["xtick.labelsize"] = FONTSIZE
+matplotlib.rcParams["ytick.labelsize"] = FONTSIZE
+matplotlib.rcParams["axes.labelsize"] = FONTSIZE
+matplotlib.rcParams["axes.titlesize"] = FONTSIZE
+matplotlib.rcParams["savefig.bbox"] = "tight"
+matplotlib.rcParams["savefig.pad_inches"] = 0.1
+matplotlib.rcParams["lines.linewidth"] = 2
+matplotlib.rcParams["axes.linewidth"] = 1.6
+
+
+class NTBNet(torch.nn.Module):
+    """
+    No-Transaction Band network.
+
+    Parameters
+    ----------
+    - in_features : int, default 3
+        Number of input features.
+
+    Examples
+    --------
+    >>> _ = torch.manual_seed(42)
+    >>> m = NTBNet()
+    >>> x = torch.tensor([
+    ...     [-0.01, 0.1, 0.2],
+    ...     [ 0.00, 0.1, 0.2],
+    ...     [ 0.01, 0.1, 0.2]])
+    >>> prev = torch.full_like(x[:, 0], 0.5)
+    >>> m(x, prev)
+    tensor([..., ..., ...], grad_fn=<SWhereBackward>)
+    """
+
+    def __init__(self, in_features=3):
+        super().__init__()
+        self.net = Net(in_features, 2)
+
+    def forward(self, x, prev):
+        delta = bs_delta(x[:, 0], x[:, 1], x[:, 2])
+        width = self.net(x)
+
+        lower = delta - fn.leaky_relu(width[:, 0])
+        upper = delta + fn.leaky_relu(width[:, 1])
+
+        return clamp(prev, lower, upper)
+
+
+class FFNet(torch.nn.Module):
+    """
+    Feed-forward network with BS delta.
+    Parameters
+    ----------
+    - in_features : int, default 3
+        Number of input features.
+    Examples
+    --------
+    >>> _ = torch.manual_seed(42)
+    >>> m = FFNet(3)
+    >>> x = torch.tensor([
+    ...     [-0.01, 0.1, 0.2],
+    ...     [ 0.00, 0.1, 0.2],
+    ...     [ 0.01, 0.1, 0.2]])
+    >>> prev = torch.full_like(x[:, 0], 0.5)
+    >>> m(x, prev)
+    tensor([..., ..., ...], grad_fn=<AddBackward0>)
+    """
+
+    def __init__(self, in_features=3):
+        super().__init__()
+        self.net = Net(in_features + 1, 1)
+
+    def forward(self, x, prev):
+        delta = bs_delta(x[:, 0], x[:, 1], x[:, 2])
+
+        x = torch.cat((x, prev.reshape(-1, 1)), 1)
+        x = self.net(x).reshape(-1)
+        x = torch.tanh(x)
+
+        return delta + x
+
+
+if __name__ == "__main__":
+    DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # ---
+
+    DEVICE
+
+    # ---
+
+    N_PATHS = 50000
+    N_SIMULATIONS = 200
+
+    # ---
+
+    def to_numpy(tensor: torch.Tensor) -> np.array:
+        return tensor.cpu().detach().numpy()
+
+    # ---
+
+    def european_payoff(spot, strike=1.0) -> torch.Tensor:
+        """
+        Return payoff of a European option.
+
+        Parameters
+        ----------
+        spot : torch.Tensor, shape (n_steps, n_paths)
+
+        Returns
+        -------
+        payoff : torch.Tensor, shape (n_paths, )
+        """
+        return fn.relu(spot[-1, :] - strike)
+
+    def lookback_payoff(spot, strike=1.03) -> torch.Tensor:
+        """
+        Return payoff of a lookback option.
+
+        Parameters
+        ----------
+        spot : torch.Tensor, shape (n_steps, n_paths)
+
+        Returns
+        -------
+        payoff : torch.Tensor, shape (n_paths, )
+        """
+        return fn.relu(torch.max(spot, dim=0).values - strike)
+
+    # ---
+
+    def compute_pnl(
+        model,
+        liability,
+        c,
+        n_paths=N_PATHS,
+        maturity=30 / 365,
+        dt=1 / 365,
+        volatility=0.2,
+    ) -> torch.Tensor:
+        """
+        Return profit-loss distribution after hedging.
+
+        Parameters
+        ----------
+        - model : torch.nn.Module
+            Model to fit.
+        - liability : {"european", "lookback"}
+            Liability to hedge.
+        - c : float
+            Transaction cost of underlying asset.
+
+        Returns
+        -------
+        pnl : torch.Tensor, shape (n_paths,)
+        """
+        spot = generate_gbm(
+            n_paths, maturity=maturity, dt=dt, volatility=volatility, device=DEVICE
+        )
+        n_steps = spot.shape[0]
+
+        prev = torch.zeros((n_paths,)).to(DEVICE)
+
+        if liability == "european":
+            pnl = -european_payoff(spot)
+        if liability == "lookback":
+            pnl = -lookback_payoff(spot)
+
+        for n in range(n_steps - 1):
+            # log-moneyness, time_expiry, volatility
+            logm = torch.log(spot[n, :]).reshape(-1, 1)
+            time = torch.full_like(logm, maturity - n * dt)
+            vola = torch.full_like(logm, volatility)
+            x = torch.cat([logm, time, vola], 1)
+
+            hedge = model(x, prev)
+
+            pnl += hedge * (spot[n + 1] - spot[n])
+            pnl -= c * torch.abs(hedge - prev) * spot[n]
+
+            prev = hedge
+
+        return pnl
+
+    # ---
+
+    # Definition of NTBNet comes here
+
+    # class NTBNet(torch.nn.Module):
+    #     ...
+
+    # ---
+
+    # Definition of FFNet comes here
+
+    # class FFNet(torch.nn.Module):
+    #     ...
+
+    # ---
+
+    torch.manual_seed(42)
+    model_ntb = NTBNet().to(DEVICE)
+    torch.manual_seed(42)
+    model_ffn = FFNet().to(DEVICE)
+
+    # ---
+
+    torch.manual_seed(42)
+    pnl_ntb = compute_pnl(model_ntb, "european", c=1e-3)
+    torch.manual_seed(42)
+    pnl_ffn = compute_pnl(model_ffn, "european", c=1e-3)
+
+    # ---
+
+    plt.hist(
+        to_numpy(pnl_ntb),
+        bins=100,
+        range=(-0.04, -0.01),
+        alpha=0.6,
+        label="No-transaction band Network",
+    )
+    plt.hist(
+        to_numpy(pnl_ffn),
+        bins=100,
+        range=(-0.04, -0.01),
+        alpha=0.6,
+        label="Feed-forward Network",
+    )
+    plt.title(
+        "Profit-loss histogram of 50000 price paths "
+        "for a European option (before fit)"
+    )
+    plt.xlabel("Profit-loss")
+    plt.ylabel("Number of events")
+    plt.legend()
+    plt.show()
+
+    # ---
+
+    def fit(model, liability, c, n_simulations=N_SIMULATIONS) -> list:
+        """
+        Fit a model to hedge the given liability.
+
+        Parameters
+        ----------
+        - model : torch.nn.Module
+            Model to fit.
+        - liability : {"european", "lookback"}
+            Liability to hedge.
+        - c : float, default 0.0
+            Transaction cost of underlying asset.
+        - n_simulations : int, default 200
+            Number of simulations.
+
+        Returns
+        -------
+        history : list[float]
+            Training history.
+        """
+        optim = Adam(model.parameters())
+
+        history = []
+        iterations = tqdm(range(n_simulations))
+
+        for i in iterations:
+            optim.zero_grad()
+            pnl = compute_pnl(model, liability, c=c)
+            loss = entropic_loss(pnl)
+            loss.backward()
+            optim.step()
+
+            iterations.desc = f"loss={loss:.5f}"
+            history.append(loss.item())
+
+        return history
+
+    # ---
+
+    torch.manual_seed(42)
+    history_ntb = fit(model_ntb, "european", c=1e-3)
+    torch.manual_seed(42)
+    history_ffn = fit(model_ffn, "european", c=1e-3)
+
+    # ---
+
+    plt.figure()
+    plt.plot(history_ntb, label="No-transaction band Network")
+    plt.plot(history_ffn, label="Feed-forward Network")
+    plt.xlabel("Number of simulations")
+    plt.ylabel("Loss (Negative of expected utility)")
+    plt.title("Learning history for a European option")
+    plt.legend()
+    plt.show()
+
+    # ---
+
+    torch.manual_seed(42)
+    pnl_ntb = compute_pnl(model_ntb, "european", c=1e-3)
+    torch.manual_seed(42)
+    pnl_ffn = compute_pnl(model_ffn, "european", c=1e-3)
+
+    # ---
+
+    plt.figure()
+    plt.hist(
+        to_numpy(pnl_ntb),
+        bins=100,
+        range=(-0.12, 0.01),
+        alpha=0.6,
+        label="No-transaction band Network",
+    )
+    plt.hist(
+        to_numpy(pnl_ffn),
+        bins=100,
+        range=(-0.12, 0.01),
+        alpha=0.6,
+        label="Feed-forward Network",
+    )
+    plt.title(
+        "Profit-loss histogram of 50000 price paths "
+        "for a European option (after fit)"
+    )
+    plt.xlabel("Profit-loss")
+    plt.ylabel("Number of events")
+    plt.legend()
+    plt.show()
+
+    # ---
+
+    def price(model, liability, c, n_times=20) -> float:
+        """
+        Evaluate a price of the given derivative.
+
+        Parameters
+        ----------
+        - model : torch.nn.Module
+            Model to fit.
+        - liability : {"european", "lookback"}
+            Liability to hedge.
+        - c : float, default 0.0
+            Transaction cost of underlying asset.
+        - n_times : int, default 200
+            Number of simulations.
+
+        Returns
+        -------
+        price : torch.Tensor, shape (,)
+        """
+        with torch.no_grad():
+            p = lambda: -cash_equivalent(compute_pnl(model, liability, c))
+            return torch.mean(torch.stack([p() for _ in range(n_times)])).item()
+
+    # ---
+
+    def fit_price(model, liability, c):
+        history = fit(model, liability, c)
+        p = price(model, liability, c)
+        return history, p
+
+    # ---
+
+    torch.manual_seed(42)
+    price_ntb = price(model_ntb, "european", c=1e-3)
+    torch.manual_seed(42)
+    price_ffn = price(model_ffn, "european", c=1e-3)
+
+    # ---
+
+    print("Price by no-transaction band network:\t", price_ntb)
+    print("Price by feed-forward band network:\t", price_ffn)
+
+    # ---
+
+    print("Reduced price:\t", f"{(price_ffn - price_ntb) / price_ffn * 100:.4} %")
+
+    torch.manual_seed(42)
+    model_ntb = NTBNet().to(DEVICE)
+    torch.manual_seed(42)
+    model_ffn = FFNet().to(DEVICE)
+
+    # ---
+
+    torch.manual_seed(42)
+    history_ntb, price_ntb = fit_price(model_ntb, "lookback", c=1e-3)
+    torch.manual_seed(42)
+    history_ffn, price_ffn = fit_price(model_ffn, "lookback", c=1e-3)
+
+    # ---
+
+    plt.figure()
+    plt.plot(history_ntb, label="No-transaction band Network")
+    plt.plot(history_ffn, label="Feed-forward Network")
+    plt.xlabel("Number of simulations")
+    plt.ylabel("Loss (Negative of expected utility)")
+    plt.title("Learning history for a lookback option")
+    plt.legend()
+    plt.show()
---
+++ utils.py
@@ -0,0 +1,224 @@
+import torch
+from torch.distributions.normal import Normal
+from torch.nn import Linear
+from torch.nn import ReLU
+
+
+def bs_delta(log_moneyness, time_expiry, volatility) -> torch.Tensor:
+    """
+    Return Black-Scholes delta of European option.
+
+    Parameters
+    ----------
+    - s : torch.Tensor
+        log moneyness.
+    - t : torch.Tensor
+        Time to expiry.
+    - v : torch.Tensor
+        Volatility.
+
+    Returns
+    -------
+    delta : torch.Tensor
+
+    Examples
+    --------
+    >>> bs_delta([-0.01, 0.00, 0.01], 0.1, 0.2)
+    tensor([0.4497, 0.5126, 0.5752])
+    """
+    s, t, v = map(torch.as_tensor, (log_moneyness, time_expiry, volatility))
+    normal = Normal(torch.tensor(0.0), torch.tensor(1.0))
+    return normal.cdf((s + (v ** 2 / 2) * t) / (v * torch.sqrt(t)))
+
+
+def clamp(x, min_value, max_value) -> torch.Tensor:
+    """
+    Clamp all elements in the input tensor into the range [`min_value`, `max_value`]
+    and return a resulting tensor.
+    The bounds `min_value` and `max_value` can be tensors.
+
+    If min_value <= max_value:
+
+        out = min_value if x < min_value
+              x if min_value <= x <= max_value
+              max_value if x > max_value
+
+    If min_value > max_value:
+
+        out = (min_value + max_value) / 2
+
+    Parameters
+    ----------
+    - x : torch.Tensor, shape (*)
+        The input tensor.
+    - min_value : float or torch.Tensor, default None
+        Lower-bound of the range to be clamped to.
+    - max_value : float or torch.Tensor, default None
+        Upper-bound of the range to be clamped to.
+
+    Returns
+    -------
+    out : torch.tensor, shape (*)
+        The output tensor.
+
+    Examples
+    --------
+    >>> x = torch.linspace(-2, 12, 15) * 0.1
+    >>> x
+    tensor([-0.2000, -0.1000,  0.0000,  0.1000,  0.2000,  0.3000,  0.4000,  0.5000,
+             0.6000,  0.7000,  0.8000,  0.9000,  1.0000,  1.1000,  1.2000])
+    >>> clamp(x, 0.0, 1.0)
+    tensor([0.0000, 0.0000, 0.0000, 0.1000, 0.2000, 0.3000, 0.4000, 0.5000, 0.6000,
+            0.7000, 0.8000, 0.9000, 1.0000, 1.0000, 1.0000])
+
+    >>> x = torch.tensor([1.0, 0.0])
+    >>> clamp(x, [0.0, 1.0], 0.0)
+    tensor([0.0000, 0.5000])
+    """
+    if min_value is not None:
+        min_value = torch.as_tensor(min_value)
+        x = torch.max(x, min_value)
+    if max_value is not None:
+        max_value = torch.as_tensor(max_value)
+        x = torch.min(x, max_value)
+    if min_value is not None and max_value is not None:
+        x = torch.where(min_value < max_value, x, (min_value + max_value) / 2)
+    return x
+
+
+class Net(torch.nn.ModuleList):
+    """
+    Feed-forward neural network.
+
+    Parameters
+    ----------
+    - in_features : int
+        Number of input features.
+    - out_features : int
+        Number of output features.
+    - n_layers : int
+        Number of hidden layers.
+    - n_units : int, default
+        Number of units in each hidden layer.
+
+    Examples
+    --------
+    >>> net = Net(3, 1)
+    >>> net
+    Net(
+      (0): Linear(in_features=3, out_features=32, bias=True)
+      (1): ReLU()
+      (2): Linear(in_features=32, out_features=32, bias=True)
+      (3): ReLU()
+      (4): Linear(in_features=32, out_features=32, bias=True)
+      (5): ReLU()
+      (6): Linear(in_features=32, out_features=32, bias=True)
+      (7): ReLU()
+      (8): Linear(in_features=32, out_features=1, bias=True)
+    )
+    >>> net(torch.empty(2, 3))
+    tensor([[...],
+            [...]], grad_fn=<AddmmBackward>)
+    """
+
+    def __init__(self, in_features, out_features, n_layers=4, n_units=32):
+        super().__init__()
+        for n in range(n_layers):
+            i = in_features if n == 0 else n_units
+            self.append(Linear(i, n_units))
+            self.append(ReLU())
+        self.append(Linear(n_units, out_features))
+
+    def forward(self, x):
+        for layer in self:
+            x = layer(x)
+        return x
+
+
+def generate_gbm(
+    n_paths, maturity=30 / 365, dt=1 / 365, volatility=0.2, device=None
+) -> torch.Tensor:
+    """
+    Return geometric Brownian motion of shape (n_steps, n_paths).
+
+    Parameters
+    ----------
+    - n_paths : int
+        Number of price paths.
+    - maturity : float, default 30 / 365
+        Length of simulated period.
+    - dt : float, default 1 / 365
+        Interval of time steps.
+    - volatility : float, default 0.2
+        Volatility of asset price.
+
+    Returns
+    -------
+    gbm : torch.Tensor, shape (N_STEPS, N_PATHS)
+        Here `N_PATH = int(maturity / dt)`.
+
+    Examples
+    --------
+    >>> _ = torch.manual_seed(42)
+    >>> generate_gbm(3, maturity=5 / 365)
+    tensor([[1.0000, 1.0000, 1.0000],
+            [1.0023, 0.9882, 0.9980],
+            [1.0257, 0.9816, 1.0027],
+            [1.0285, 0.9870, 1.0112],
+            [1.0405, 0.9697, 1.0007]])
+    """
+    randn = torch.randn((int(maturity / dt), n_paths))
+    randn[0, :] = 0.0
+    bm = volatility * torch.sqrt(torch.tensor(dt)) * randn.cumsum(0)
+    t = torch.linspace(0, maturity, int(maturity / dt)).reshape(-1, 1)
+    gbm = torch.exp(bm - (volatility ** 2) * t / 2)
+    gbm = gbm.to(device=device)
+    return gbm
+
+
+def entropic_loss(pnl) -> torch.Tensor:
+    """
+    Return entropic loss function, which is a negative of
+    the expected entropic utility:
+
+        loss(pnl) = -E[u(pnl)], u(x) = -exp(-x)
+
+    Parameters
+    ----------
+    pnl : torch.Tensor, shape (*)
+        Profit-loss distribution.
+
+    Returns
+    -------
+    loss : torch.Tensor, shape (,)
+
+    Examples
+    --------
+    >>> pnl = -torch.arange(4.0)
+    >>> entropic_loss(pnl)
+    tensor(7.7982)
+    """
+    return -torch.mean(-torch.exp(-pnl))
+
+
+def cash_equivalent(pnl) -> torch.Tensor:
+    """
+    Return cash equivalent of profit-loss distribution
+    with respect to entropic exponential utility.
+
+    Parameters
+    ----------
+    pnl : torch.Tensor, shape (*)
+        Profit-loss distribution.
+
+    Returns
+    -------
+    cash_equivaluent : torch.Tensor, shape (,)
+
+    Examples
+    --------
+    >>> pnl = -torch.arange(4.0)
+    >>> cash_equivalent(pnl)
+    tensor(-2.0539)
+    """
+    return -torch.log(entropic_loss(pnl))
