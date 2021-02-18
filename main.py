1e4a233e2f9f5fcb35f30805628d32b5b0d7dabe
Shota Imaki <shota.imaki.0801@gmail.com>
2021-02-18 11:24:47 +0900

    Add markdown

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+"""
+# No-Transaction Band Network
+"""
+
+# %%
 import typing
 
 import matplotlib
@@ -51,6 +56,13 @@ def to_numpy(tensor: torch.Tensor) -> np.array:
     return tensor.cpu().detach().numpy()
 
 # %%
+"""
+## Prepare derivatives
+
+We consider a European option and a lookback option.
+"""
+
+# %%
 def european_option_payoff(prices: torch.Tensor, strike=1.0) -> torch.Tensor:
     """
     Return the payoff of a European option.
@@ -66,7 +78,7 @@ def european_option_payoff(prices: torch.Tensor, strike=1.0) -> torch.Tensor:
     """
     return fn.relu(prices[-1, :] - strike)
 
-
+# %%
 def lookback_option_payoff(prices: torch.Tensor, strike=1.03) -> torch.Tensor:
     """
     Return the payoff of a lookback option.
@@ -81,6 +93,22 @@ def lookback_option_payoff(prices: torch.Tensor, strike=1.03) -> torch.Tensor:
     payoff : torch.Tensor, shape (n_paths, )
     """
     return fn.relu(torch.max(prices, dim=0).values - strike)
+
+# %%
+"""
+## European Option
+"""
+
+# %%
+"""
+### Compute profit and loss after hedging
+
+A dealer sells a derivative to its customer and obliges a liability to settle the payoff at maturity.
+
+The dealer may hedge the risk of this liability by trading an underlying asset of the derivative based on `hedging_model`.
+
+The resulting profit and loss is obtained by adding up the payoff to the customer, capital gains from the underlying asset, and the transaction cost.
+"""
 
 # %%
 def compute_profit_and_loss(
@@ -132,6 +160,21 @@ def compute_profit_and_loss(
     return pnl
 
 # %%
+"""
+### Create hedging models
+
+Now let us create `hedging_model` as `torch.nn.Module`.
+
+We employ two models here:
+* **No-Transaction Band Network**:
+    - A multi-layer perceptron outputs a no-transaction band and the next hedge ratio is obtained by clamping the current hedge ratio into this band.
+    - Two outputs of the multi-layer perceptron are applied with [`LeakyReLU`](https://pytorch.org/docs/stable/generated/torch.nn.LeakyReLU.html#torch.nn.LeakyReLU) and then added and subtracted to the Black–Scholes’ delta to get the upper- and lower-bound of the no-transaction band, respectively.
+* **Feed-forward network**:
+    - A multi-layer perception uses the current hedge ratio as an input to compute the next hedge ratio.
+    - The output of a multi-layer perceptron is applied with [`tanh`](https://pytorch.org/docs/stable/generated/torch.nn.Tanh.html#torch.nn.Tanh) function and then added to Black–Scholes’ delta to get the next hedge ratio.
+"""
+
+# %%
 class NoTransactionBandNet(torch.nn.Module):
     """
     No-transaction band network.
@@ -156,11 +199,11 @@ class NoTransactionBandNet(torch.nn.Module):
 
     def __init__(self, in_features=3):
         super().__init__()
-        self.net = MultiLayerPerceptron(in_features, 2)
+        self.mlp = MultiLayerPerceptron(in_features, 2)
 
     def forward(self, x, prev):
         no_cost_delta = european_option_delta(x[:, 0], x[:, 1], x[:, 2])
-        band_width = self.net(x)
+        band_width = self.mlp(x)
 
         lower = no_cost_delta - fn.leaky_relu(band_width[:, 0])
         upper = no_cost_delta + fn.leaky_relu(band_width[:, 1])
@@ -192,13 +235,13 @@ class FeedForwardNet(torch.nn.Module):
 
     def __init__(self, in_features=3):
         super().__init__()
-        self.net = MultiLayerPerceptron(in_features + 1, 1)
+        self.mlp = MultiLayerPerceptron(in_features + 1, 1)
 
     def forward(self, x, prev):
         no_cost_delta = european_option_delta(x[:, 0], x[:, 1], x[:, 2])
 
         x = torch.cat((x, prev.reshape(-1, 1)), 1)
-        x = self.net(x).reshape(-1)
+        x = self.mlp(x).reshape(-1)
         x = torch.tanh(x)
 
         return no_cost_delta + x
@@ -240,6 +283,13 @@ plt.legend()
 plt.show()
 
 # %%
+"""
+### Fit hedging models
+
+We train hedging models so that they minimize the `entropic_loss`, or equivalently, maximize the expected utility.
+"""
+
+# %%
 def fit(
     hedging_model: torch.nn.Module,
     payoff: typing.Callable[[torch.Tensor], torch.Tensor],
@@ -270,7 +320,7 @@ def fit(
     history = []
     iterations = tqdm(range(n_epochs))
 
-    for i in iterations:
+    for _ in iterations:
         optim.zero_grad()
         pnl = compute_profit_and_loss(hedging_model, payoff, cost=cost)
         loss = entropic_loss(pnl)
@@ -282,12 +332,16 @@ def fit(
 
     return history
 
-
 # %%
 torch.manual_seed(42)
 history_ntb = fit(model_ntb, european_option_payoff, cost=1e-3)
 torch.manual_seed(42)
 history_ffn = fit(model_ffn, european_option_payoff, cost=1e-3)
+
+# %%
+"""
+The learning histories below show that the no-transaction band network can be trained much quicker than the ordinary feed-forward network.
+"""
 
 # %%
 plt.figure()
@@ -298,6 +352,13 @@ plt.ylabel("Loss (Negative of expected utility)")
 plt.title("Learning history for a European option")
 plt.legend()
 plt.show()
+
+# %%
+"""
+### Compute the profit-loss distributions after hedging
+
+The histograms of the profit and loss after hedging look like this.
+"""
 
 # %%
 torch.manual_seed(42)
@@ -328,6 +389,15 @@ plt.xlabel("Profit-loss")
 plt.ylabel("Number of events")
 plt.legend()
 plt.show()
+
+# %%
+"""
+### Evaluate the premium of the derivative
+
+Now, we are ready to define the premium of the derivative.
+
+Premium of a derivative is defined as the guaranteed amount of cash which is as preferable as the profit-loss after hedging in terms of the exponential utility.
+"""
 
 # %%
 def evaluate_premium(
@@ -361,12 +431,16 @@ def evaluate_premium(
         ).item()
         return float(np.mean([p() for _ in range(n_times)]))
 
-
 # %%
 torch.manual_seed(42)
 premium_ntb = evaluate_premium(model_ntb, european_option_payoff, cost=1e-3)
 torch.manual_seed(42)
 premium_ffn = evaluate_premium(model_ffn, european_option_payoff, cost=1e-3)
+
+# %%
+"""
+The no-transaction band network allows for a cheaper price.
+"""
 
 # %%
 print("Premium evaluated by no-transaction band network :\t", premium_ntb)
@@ -377,10 +451,20 @@ premium_reduced = (premium_ffn - premium_ntb) / premium_ffn * 100
 print("Reduced premium :\t", f"{premium_reduced:.4f} %")
 
 # %%
+"""
+## Lookback Option
+"""
+
+# %%
 torch.manual_seed(42)
 model_ntb = NoTransactionBandNet().to(DEVICE)
 torch.manual_seed(42)
 model_ffn = FeedForwardNet().to(DEVICE)
+
+# %%
+"""
+### Fit hedging models
+"""
 
 # %%
 torch.manual_seed(42)
@@ -399,10 +483,20 @@ plt.legend()
 plt.show()
 
 # %%
+"""
+### Compute the profit-loss distributions after hedging
+"""
+
+# %%
 torch.manual_seed(42)
 premium_ntb = evaluate_premium(model_ntb, lookback_option_payoff, cost=1e-3)
 torch.manual_seed(42)
 premium_ffn = evaluate_premium(model_ffn, lookback_option_payoff, cost=1e-3)
+
+# %%
+"""
+### Evaluate the premium of the derivative
+"""
 
 # %%
 print("Premium evaluated by no-transaction band network :\t", premium_ntb)
